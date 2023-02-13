98a8b018be369e96859a6652d272ab02ef37d201
aobise <splasherdon@gmail.com>
2023-02-13 00:35:24 +0000

    2commit

---
+++ imrich.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+echo "sudo apt install default-jre"
+sleep 5
+echo
+
+echo "sudo apt install default-jdk"
+sleep 5
+echo "what is my name?"
+sleep 5
+echo "processing answer......"
+sleep 5
+echo "thank you for your answer"
+sleep 5
+echo "im a millionaire"
